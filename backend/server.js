9c446ccc5f4639b534ad179dcd418b1f68674871
[Samridhi Gupta] <samridhigupta1944@gmail.com>
2026-05-07 21:50:45 +0530

    hi

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('(.*)', (req, res) =>
+  app.get('*splat', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'))
   );
 } else {
